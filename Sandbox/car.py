ae947d67437fb55f7fed0b98b331299c5ff99349
Peter Anema <email@peteranema.nl>
2022-06-30 12:33:06 +0200

    Update

--- Sandbox/car.py
+++ Sandbox/car.py
@@ -1,22 +1,22 @@
 class Car:
 
-    def __init__(self, make, type, color):
+    def __init__(self, make, model, color, mileage = 0):
         self._make = make
-        self._type = type
+        self._model = model
         self._color = color
-        self._mileage = 0
+        self._mileage = mileage
         self._started = False
 
     def info(self):
-        print( f'This great {self._color} {self._make} {self._type} as driven {self._mileage}km.' )
+        print( f'This great {self._color} {self._make} {self._model} as driven {self._mileage}km.' )
         if self._started:
             print( f'The engine is still running.')
 
     def __str__(self):
-        return f'Car: {self._color} {self._make} {self._type} mileage: {self._mileage}km.'
+        return f'Car: {self._color} {self._make} {self._model} mileage: {self._mileage}km.'
 
     def __repr__(self):
-        return f"Car('{self._make}', '{self._type}', '{self._color}')"
+        return f"Car('{self._make}', '{self._model}', '{self._color}')"
 
     def drive(self, km):
         if self._started:
@@ -33,13 +33,19 @@ class Car:
     def __del__(self):
         print('The car has been demolished. Too bad.')
 
+
+
+    
 # -------------------------------------------------------
 
-my_car = Car('Renault', 'Megane', 'metalic brown')
+if __name__ == '__main__':
+    
+    my_car = Car('Renault', 'Megane', 'metalic brown')
 
-my_car.start()
-my_car.drive(200)
+    my_car.start()
+    my_car.drive(200)
 
-print(my_car)
+    print(my_car)
 
-del my_car
+#    del my_car
+
---
+++ demo turtle2.py
@@ -0,0 +1,18 @@
+import turtle
+
+turtle.forward(100)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(45)
+turtle.forward(140)
+turtle.left(90)
+turtle.forward(140)
+turtle.right(45)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+
+turtle.done()
+
+
