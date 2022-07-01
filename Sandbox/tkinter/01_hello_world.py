dde01a12a139de7aa8e11d1e9480f9cd614a8f70
Peter Anema <email@peteranema.nl>
2022-07-01 09:25:03 +0200

    Update

--- Sandbox/tkinter/01_hello_world.py
+++ Sandbox/tkinter/01_hello_world.py
@@ -1,6 +1,7 @@
-from tkinter import *
-from tkinter import ttk
+import tkinter as tk
 
-root = Tk()
-ttk.Button(root, text="Hello World").grid()
+root = tk.Tk()
+
+tk.Button(root, text="Hello World").grid()
+
 root.mainloop()
